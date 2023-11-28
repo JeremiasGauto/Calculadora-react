e1ac81f42cbf5ebb3c14912aefb5b3d505d5c780
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-11-28 19:09:40 -0300

    armando body de calculadora y botones

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,27 @@ function App() {
   return (
     <>
       <div>
-        <div className='cuerpo-calculadora'>
-            <Boton>1</Boton>
-            <Boton>2</Boton>
-            <Boton>3</Boton>
-            <Boton>+</Boton>
-            <Boton>4</Boton>
-            <Boton>5</Boton>
-            <Boton>6</Boton>
-            <Boton>=</Boton>
-            <Boton>7</Boton>
-            <Boton>8</Boton>
-            <Boton>9</Boton>
-            <Boton>C</Boton>
-          <Boton>0</Boton>
+        <div className="cuerpo-calculadora">
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>X</Boton>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
           <Boton>-</Boton>
-          <Boton>/</Boton>
-          </div>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+          <Boton>AC</Boton>
+          <Boton>0</Boton>
+          <Boton></Boton>
+          <Boton>=</Boton>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App
